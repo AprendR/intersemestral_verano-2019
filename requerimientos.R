1970253215d4490b672b76e76e51509e016c5789
nekrum <nekrum@gmail.com>
2019-07-14 15:26:51 -0500

    Agrega archivo de requerimientos, librerías necesarias.

---
+++ requerimientos.R
@@ -0,0 +1,8 @@
+install.packages("devtools")
+install.packages("foreign")
+install.packages("gapminder")
+install.packages("Lahman")
+install.packages("nycflights13")
+install.packages("remotes")
+install.packages("tidyverse")
+
